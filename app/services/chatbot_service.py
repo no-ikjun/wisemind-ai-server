7b2afbfe63392449d0ec5a475beee5a9a7a36875
최익준 <96350366+no-ikjun@users.noreply.github.com>
2025-07-11 19:55:15 +0900

    Update chatbot_service.py (클로바 활용 함수 추가)

--- app/services/chatbot_service.py
+++ app/services/chatbot_service.py
@@ -2,11 +2,56 @@ import os
 import openai
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+API_KEY = os.getenv("NCLOVA_API_KEY")
+API_URL = "https://clovastudio.stream.ntruss.com/testapp/v1/chat-completions/HCX-003"
+
+HEADERS = {
+    "X-NCP-CLOVASTUDIO-API-KEY": API_KEY,
+    "X-NCP-APIGW-API-KEY": API_KEY,
+    "Content-Type": "application/json",
+}
 
 session_store = {}
 
+# CLOVA 활용 채팅
 def get_chatbot_response(session_id: str, user_message: str) -> str:
+    if session_id not in session_store:
+        session_store[session_id] = [
+            {"role": "system", "content": "너는 Wisemind의 투자 분석 도우미야. 간결하고 정확하게 답변해."}
+        ]
+
+    conversation = session_store[session_id]
+    conversation.append({"role": "user", "content": user_message})
+
+    try:
+        payload = {
+            "messages": conversation,
+            "topP": 0.8,
+            "topK": 0,
+            "temperature": 0.7,
+            "repeatPenalty": 5.0,
+            "stopBefore": [],
+            "includeAiFilters": False,
+            "seed": 0,
+        }
+
+        response = requests.post(API_URL, headers=HEADERS, json=payload)
+        response.raise_for_status()
+        assistant_reply = response.json()["result"]["message"]["content"].strip()
+
+        conversation.append({"role": "assistant", "content": assistant_reply})
+        return assistant_reply
+
+    except Exception as e:
+        print(f"HyperCLOVA X Error: {e}")
+        return "답변 생성 실패"
+
+
+
+client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+
+#OpenAI 활용 채팅
+def get_chatbot_response_openai(session_id: str, user_message: str) -> str:
     if session_id not in session_store:
         session_store[session_id] = [
             {"role": "system", "content": "너는 Wisemind의 투자 분석 도우미야. 간결하고 정확하게 답변해."}
